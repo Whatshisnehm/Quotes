7c5244cbee59d6a81b289c9186d5850e24c8ceb4
Peter Frost <peter.frost@aimsmarter.co.uk>
2022-02-01 16:10:43 +0000

    pickled

--- simple_ruby.rb
+++ simple_ruby.rb
@@ -1,1 +1,9 @@
 p "Hi guys, how you doing?"
+feeling = gets.chomp
+p "Oh right, you're #{feeling}, can we make that better?"
+better = gets.chomp
+if better.downcase.include? "yes"
+	p "Yeah we can!  We can make you better than #{feeling}, we can make you a train."
+else
+	p "Well come back when you're more receptive to feeling better than #{feeling}."
+end
